e4fe7e5f9e59bce1cef86f4f028374c365a0736c
Krisztian Csepregi <krisztian.csepregi@benestudio.co>
2020-03-24 16:32:03 +0100

    Add multiple attachment

--- RNMail/RNMail.m
+++ RNMail/RNMail.m
@@ -130,6 +130,74 @@ RCT_EXPORT_METHOD(mail:(NSDictionary *)options
             [mail addAttachmentData:fileData mimeType:mimeType fileName:attachmentName];
         }
 
+                if (options[@"attachments"]){
+            NSArray *attachments = [RCTConvert NSArray:options[@"attachments"]];
+
+            for(NSDictionary *attachment in attachments) {
+                if (attachment && attachment[@"path"] && attachment[@"type"]) {
+                    NSString *attachmentPath = [RCTConvert NSString:attachment[@"path"]];
+                    NSString *attachmentType = [RCTConvert NSString:attachment[@"type"]];
+                    NSString *attachmentName = [RCTConvert NSString:attachment[@"name"]];
+
+                    NSFileManager *fileManager = [NSFileManager defaultManager];
+                    if (![fileManager fileExistsAtPath:attachmentPath]){
+                        callback(@[[NSString stringWithFormat: @"attachment file with path '%@' does not exist", attachmentPath]]);
+                        return;
+                    }
+                    
+                    // Set default filename if not specificed
+                    if (!attachmentName) {
+                        attachmentName = [[attachmentPath lastPathComponent] stringByDeletingPathExtension];
+                    }
+
+                    // Get the resource path and read the file using NSData
+                    NSData *fileData = [NSData dataWithContentsOfFile:attachmentPath];
+
+                    // Determine the MIME type
+                    NSString *mimeType;
+
+                    if (attachmentType) {
+                        /*
+                        * Add additional mime types and PR if necessary. Find the list
+                        * of supported formats at http://www.iana.org/assignments/media-types/media-types.xhtml
+                        */
+                        NSDictionary *supportedMimeTypes = @{
+                            @"jpeg" : @"image/jpeg",
+                            @"jpg" : @"image/jpeg",
+                            @"png" : @"image/png",
+                            @"doc" : @"application/msword",
+                            @"docx" : @"application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                            @"ppt" : @"application/vnd.ms-powerpoint",
+                            @"pptx" : @"application/vnd.openxmlformats-officedocument.presentationml.presentation",
+                            @"html" : @"text/html",
+                            @"csv" : @"text/csv",
+                            @"pdf" : @"application/pdf",
+                            @"vcard" : @"text/vcard",
+                            @"json" : @"application/json",
+                            @"zip" : @"application/zip",
+                            @"text" : @"text/*",
+                            @"mp3" : @"audio/mpeg",
+                            @"wav" : @"audio/wav",
+                            @"aiff" : @"audio/aiff",
+                            @"flac" : @"audio/flac",
+                            @"ogg" : @"audio/ogg",
+                            @"xls" : @"application/vnd.ms-excel",
+                            @"ics" : @"text/calendar",
+                            @"xlsx" : @"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                        };
+                        if([supportedMimeTypes objectForKey:attachmentType]) {
+                            mimeType = [supportedMimeTypes objectForKey:attachmentType];
+                        } else {
+                            callback(@[[NSString stringWithFormat: @"Mime type '%@' for attachment is not handled", attachmentType]]);
+                            return;
+                        }
+                    }
+
+                    [mail addAttachmentData:fileData mimeType:mimeType fileName:attachmentName];
+                }
+            }
+        }
+
         UIViewController *root = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
 
         while (root.presentedViewController) {
--- android/src/main/java/com/chirag/RNMail/RNMailModule.java
+++ android/src/main/java/com/chirag/RNMail/RNMailModule.java
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.text.Html;
+import androidx.core.content.FileProvider;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -14,7 +15,14 @@ import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.Callback;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 
 /**
  * NativeModule that allows JS to open emails sending apps chooser.
@@ -53,8 +61,8 @@ public class RNMailModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void mail(ReadableMap options, Callback callback) {
-    Intent i = new Intent(Intent.ACTION_SENDTO);
-    i.setData(Uri.parse("mailto:"));
+    Intent i = new Intent(Intent.ACTION_SEND_MULTIPLE);
+    i.setType("message/rfc822");
 
     if (options.hasKey("subject") && !options.isNull("subject")) {
       i.putExtra(Intent.EXTRA_SUBJECT, options.getString("subject"));
@@ -84,14 +92,56 @@ public class RNMailModule extends ReactContextBaseJavaModule {
       i.putExtra(Intent.EXTRA_BCC, readableArrayToStringArray(bccRecipients));
     }
 
-    if (options.hasKey("attachment") && !options.isNull("attachment")) {
-      ReadableMap attachment = options.getMap("attachment");
-      if (attachment.hasKey("path") && !attachment.isNull("path")) {
-        String path = attachment.getString("path");
-        File file = new File(path);
-        Uri p = Uri.fromFile(file);
-        i.putExtra(Intent.EXTRA_STREAM, p);
+    if (options.hasKey("attachments") && !options.isNull("attachments")) {
+      ReadableArray r = options.getArray("attachments");
+      int length = r.size();
+      ArrayList<Uri> uris = new ArrayList<Uri>();
+      for (int keyIndex = 0; keyIndex < length; keyIndex++) {
+        ReadableMap clip = r.getMap(keyIndex);
+        if (clip.hasKey("path") && !clip.isNull("path")) {
+          String path = clip.getString("path");
+          Log.d ("RNMail", "Attachment file path: " + path);
+
+          File file = new File(path);
+
+          String name, suffix = "";
+          if (clip.hasKey("name"))
+            name = clip.getString("name");
+          else
+            name = file.getName();
+
+          if (clip.hasKey("type"))
+            suffix = "." + clip.getString("type");
+
+
+          file.setReadable(true, false);
+          if (file.exists()) {
+
+            if (file.length() == 0)
+              Log.d ("RNMail", "Warning, attaching empty file!");
+            // Use the FileProvider to get a content URI
+            try {
+              Uri fileUri = FileProvider.getUriForFile(
+                      getCurrentActivity(),
+                      reactContext.getPackageName() + ".fileprovider",
+                      file);
+              if (fileUri != null) {
+                // Grant temporary read permission to the content URI
+                uris.add(fileUri);
+              }
+            } catch (Exception e) {
+              String message = "There was a problem sharing the file " + file.getName();
+              Log.e("RNMail", message);
+              callback.invoke("error", message + "\n" + e.getMessage());
+            }
+          } else {
+            Log.e("RNMail", "Attachment file does not exist");
+          }
+        }
       }
+      i.setType("*/*");
+      i.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
+      i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
     }
 
     PackageManager manager = reactContext.getPackageManager();
@@ -107,7 +157,7 @@ public class RNMailModule extends ReactContextBaseJavaModule {
       try {
         reactContext.startActivity(i);
       } catch (Exception ex) {
-        callback.invoke("error");
+        callback.invoke(ex.getMessage());
       }
     } else {
       Intent chooser = Intent.createChooser(i, "Send Mail");
@@ -116,7 +166,7 @@ public class RNMailModule extends ReactContextBaseJavaModule {
       try {
         reactContext.startActivity(chooser);
       } catch (Exception ex) {
-        callback.invoke("error");
+        callback.invoke(ex.getMessage());
       }
     }
   }
